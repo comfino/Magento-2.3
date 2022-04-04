a13f8866fe7516cf400e7e59f6eef75f70922d41
akozubski@comperia.pl <akozubski@comperia.pl>
2022-04-04 17:20:14 +0200

    FN-8632 Fix bugs in cancel status mapping and in notifications processing.

--- Model/ComfinoStatusManagement.php
+++ Model/ComfinoStatusManagement.php
@@ -11,17 +11,19 @@ use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Sales\Model\Order;
 use Magento\Sales\Model\OrderRepository;
+use Psr\Log\LoggerInterface;
 
 class ComfinoStatusManagement implements ComfinoStatusManagementInterface
 {
     public const CREATED_STATUS = 'CREATED';
-    public const WAITING_FOR_FILLING_STATUS = "WAITING_FOR_FILLING";
-    public const WAITING_FOR_CONFIRMATION_STATUS = "WAITING_FOR_CONFIRMATION";
-    public const WAITING_FOR_PAYMENT_STATUS = "WAITING_FOR_PAYMENT";
-    public const ACCEPTED_STATUS = "ACCEPTED";
-    public const REJECTED_STATUS = "REJECTED";
-    public const CANCELLED_STATUS = "CANCELLED";
-    public const PAID_STATUS = "PAID";
+    public const WAITING_FOR_FILLING_STATUS = 'WAITING_FOR_FILLING';
+    public const WAITING_FOR_CONFIRMATION_STATUS = 'WAITING_FOR_CONFIRMATION';
+    public const WAITING_FOR_PAYMENT_STATUS = 'WAITING_FOR_PAYMENT';
+    public const ACCEPTED_STATUS = 'ACCEPTED';
+    public const REJECTED_STATUS = 'REJECTED';
+    public const CANCELLED_BY_SHOP_STATUS = 'CANCELLED_BY_SHOP';
+    public const CANCELLED_STATUS = 'CANCELLED';
+    public const PAID_STATUS = 'PAID';
 
     public const NEW_STATE = [
         self::CREATED_STATUS,
@@ -31,6 +33,7 @@ class ComfinoStatusManagement implements ComfinoStatusManagementInterface
 
     public const REJECTED_STATE = [
         self::REJECTED_STATUS,
+        self::CANCELLED_BY_SHOP_STATUS,
         self::CANCELLED_STATUS,
     ];
 
@@ -39,6 +42,11 @@ class ComfinoStatusManagement implements ComfinoStatusManagementInterface
         self::PAID_STATUS,
         self::WAITING_FOR_PAYMENT_STATUS,
     ];
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
     /**
      * @var OrderRepository
@@ -58,15 +66,18 @@ class ComfinoStatusManagement implements ComfinoStatusManagementInterface
     /**
      * ComfinoStatusManagement constructor.
      *
+     * @param LoggerInterface $logger
      * @param OrderRepository $orderRepository
      * @param ComfinoApplicationFactory $comfinoApplicationFactory
      * @param ApplicationResource $applicationResource
      */
     public function __construct(
+        LoggerInterface $logger,
         OrderRepository $orderRepository,
         ComfinoApplicationFactory $comfinoApplicationFactory,
         ApplicationResource $applicationResource
     ) {
+        $this->logger = $logger;
         $this->comfinoApplicationFactory = $comfinoApplicationFactory;
         $this->applicationResource = $applicationResource;
         $this->orderRepository = $orderRepository;
@@ -126,13 +137,19 @@ class ComfinoStatusManagement implements ComfinoStatusManagementInterface
     {
         $order = $this->orderRepository->get($orderId);
         $origStatus = $order->getStatus();
+
         $this->setOrderStatus($order, $status);
 
         if ($origStatus !== Order::STATE_PROCESSING && $this->isCompletedStatus($status)) {
-            $amount = $order->getGrandTotal();
+            $amount = $order->getBaseTotalDue();
             $payment = $order->getPayment();
-            $payment->registerAuthorizationNotification($amount);
-            $payment->registerCaptureNotification($amount);
+
+            if ($payment !== null) {
+                $payment->registerAuthorizationNotification($amount);
+                $payment->registerCaptureNotification($amount);
+            } else {
+                $this->logger->warning("Comfino payment not found for order $orderId.", ['orderId' => $orderId]);
+            }
         }
 
         $this->orderRepository->save($order);
