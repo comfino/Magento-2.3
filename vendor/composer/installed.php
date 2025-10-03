731670814200e44da83cdd3a6e77abf7ed7d43bb
Artur Kozubski <akozubski@comperia.pl>
2025-10-03 08:30:31 +0200

    FN-11775 Complete plugin refactoring: redesigned plugin architecture, improved errors handling, stability and reliability, code clean up - NEW RELEASE 4.0.0.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'comfino/comfinogateway',
+  ),
+  'versions' => 
+  array (
+    'comfino/comfinogateway' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
