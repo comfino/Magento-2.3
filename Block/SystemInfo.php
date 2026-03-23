85d1aadb7dc610fb638f63a7d6cd1244b05a3c32
Artur Kozubski <akozubski@comperia.pl>
2026-03-23 15:40:08 +0100

    FN-11775 Translations and module configuration management updated (diagnostic information in "Plugin diagnostics" section).

--- Block/SystemInfo.php
+++ Block/SystemInfo.php
@@ -2,6 +2,7 @@
 
 namespace Comfino\ComfinoGateway\Block;
 
+use Comfino\Api\ApiClient;
 use Comfino\ComfinoGateway\Helper\Data;
 use Comfino\ComfinoGateway\Model\Connector\Service\ApplicationService;
 use Comfino\Configuration\ConfigManager;
@@ -34,13 +35,21 @@ class SystemInfo extends Field
             $this->helper->getModuleVersion(),
             $this->helper->getShopVersion(),
             PHP_VERSION,
-            $_SERVER['SERVER_SOFTWARE'],
+            $_SERVER['SERVER_SOFTWARE'] ?? 'n/a',
             $this->helper->getDatabaseVersion()
         );
-
-        $bodyHtml = '<p><label class="label">' . __('System information') . '</label></p>';
+        $systemInfo .= '<hr><b>' . __('Comfino API host') . ':</b> ' . ApiClient::getInstance()->getApiHost();
 
         $infoMessages[] = $systemInfo;
+        $infoMessages[] = sprintf(
+            '<b>%s:</b> %s UTC',
+            __('Plugin build time'),
+            \DateTime::createFromFormat('U', (string) Data::BUILD_TS)->format('Y-m-d H:i:s')
+        );
+        $infoMessages[] = '<b>' . __('Shop domain') . ':</b> ' . htmlspecialchars($this->helper->getShopDomain());
+        $infoMessages[] = '<b>' . __('Widget key') . ':</b> ' . htmlspecialchars((string) ConfigManager::getWidgetKey());
+
+        $bodyHtml = '<p><label class="label">' . __('System information') . '</label></p>';
 
         if (ConfigManager::isSandboxMode()) {
             $warningMessages[] = __('Developer mode is active. You are using test environment.');
@@ -48,7 +57,7 @@ class SystemInfo extends Field
             if (!empty(ConfigManager::getApiKey())) {
                 if ($this->applicationService->isShopAccountActive()) {
                     $successMessages[] = __('Test account is active.');
-                } else if (count($this->applicationService->getLastErrors())) {
+                } elseif (count($this->applicationService->getLastErrors())) {
                     $errorMessages = array_merge($errorMessages, $this->applicationService->getLastErrors());
 
                     if ($this->applicationService->getLastResponseCode() === 401) {
@@ -65,7 +74,7 @@ class SystemInfo extends Field
 
             if ($this->applicationService->isShopAccountActive()) {
                 $successMessages[] = __('Production account is active.');
-            } else if (count($this->applicationService->getLastErrors())) {
+            } elseif (count($this->applicationService->getLastErrors())) {
                 $errorMessages = array_merge($errorMessages, $this->applicationService->getLastErrors());
 
                 if ($this->applicationService->getLastResponseCode() === 401) {
@@ -76,6 +85,44 @@ class SystemInfo extends Field
             }
         } else {
             $errorMessages[] = __('Production API key not present.');
+        }
+
+        if (getenv('COMFINO_DEV_ENV') === 'TRUE') {
+            $devEnvVarNames = [
+                'COMFINO_DEV_ENV',
+                'COMFINO_DEV_API_HOST',
+                'COMFINO_DEV_SDK_SCRIPT_URL',
+                'COMFINO_DEV_WIDGET_SCRIPT_URL',
+            ];
+
+            $devEnvVars = [];
+            foreach ($devEnvVarNames as $varName) {
+                $value = getenv($varName);
+                $devEnvVars[] = '<li><b>' . $varName . '</b> = "' . htmlspecialchars($value !== false ? $value : '') . '"</li>';
+            }
+
+            $hiddenOptions = ConfigManager::getConfigurationValues('hidden_settings');
+            $hiddenItems = [];
+            foreach ($hiddenOptions as $optionName => $optionValue) {
+                if (is_array($optionValue) || is_bool($optionValue)) {
+                    $optionValue = json_encode($optionValue);
+                }
+                $optionValue = (string) $optionValue;
+                if (strlen($optionValue) > 200) {
+                    $optionValue = substr($optionValue, 0, 200) . '...';
+                }
+                $hiddenItems[] = '<li><b>' . $optionName . '</b> = "' . htmlspecialchars($optionValue) . '"</li>';
+            }
+
+            $warningMessages[] = sprintf(
+                '<b>%s:</b> %s<br><b>%s:</b><ul>%s</ul><b>%s:</b><ul>%s</ul>',
+                __('Plugin dev-debug mode'),
+                ConfigManager::useDevEnvVars() ? __('active') : __('inactive'),
+                __('Environment variables'),
+                implode('', $devEnvVars),
+                __('Internal configuration'),
+                implode('', $hiddenItems)
+            );
         }
 
         foreach ($infoMessages as $infoMessage) {
